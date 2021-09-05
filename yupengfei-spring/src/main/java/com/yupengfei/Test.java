942ed837ec94545c45131f2d2cb540ef3bc6381b
ypf007 <ypf@qq.com>
2021-09-05 17:32:47 +0800

    增加 spring学习源码

--- yupengfei-spring/src/main/java/com/yupengfei/Test.java
+++ yupengfei-spring/src/main/java/com/yupengfei/Test.java
@@ -7,7 +7,7 @@ import com.yupengfei.service.UserService;
 public class Test {
     public static void main(String[] args) {
 
-        //先扫描---->创建一些单例Bean(都在构造方法里实现)
+        //先扫描---->创建一些单例Bean(都在构造方法里实现) BeanDefinition  BeanProcessor
         YupengfeiApplicationContext applicationContext = new YupengfeiApplicationContext(AppConfig.class);
 
         //UserService userService = (UserService) applicationContext.getBean("userService");
